f9d8ad80aa28147b53dd66251fa1b26972cab845
acoretchi <alexk1998.25@gmail.com>
2021-03-01 08:25:50 +0200

    some2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Count from './components/Count'
+import React, {useState} from 'react'
 
 function App() {
 
+  let [count, setCount] = useState(0)
+
+  console.log(count)
   return(
     <>
-       <Count />
+       <Count count={count}/>
+       <div className='buttonRed' onClick={()=>setCount(++count)}>UP</div>
+       <div className='buttonBlue' onClick={()=>setCount(--count)}>DOWN</div>
     </>
   );
 }
--- src/components/Count.js
+++ src/components/Count.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function Count(){
+function Count({count}){
     return(
         <div>
-            <span>Count:</span> 000
+            <span>Count:</span>{count}
         </div>
     )
 }
